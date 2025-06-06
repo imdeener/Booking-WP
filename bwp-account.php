d261be8f531776a89eb93753846fd9dc46c5c5b4
Samat Darachay <imdeener@gmail.com>
2025-06-06 09:47:12 +0700

    feat: redirect non-logged in users to login page on account pages

--- bwp-account.php
+++ bwp-account.php
@@ -15,6 +15,23 @@ if (!class_exists('WooCommerce')) {
 }
 
 /**
+ * Redirect non-logged in users to login page
+ */
+function bwp_redirect_to_login() {
+    // Only on account pages
+    if (!is_account_page()) {
+        return;
+    }
+
+    // If not logged in, redirect to login page
+    if (!is_user_logged_in()) {
+        wp_safe_redirect(wc_get_page_permalink('myaccount') . 'login/');
+        exit;
+    }
+}
+add_action('template_redirect', 'bwp_redirect_to_login', 5);
+
+/**
  * Enqueue account styles
  */
 function bwp_enqueue_account_styles() {
